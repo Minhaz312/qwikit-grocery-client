2c32e9f52e29adbecc6640bc13914cd50465b594
Mohammad Minhaz <minhazkhan312@gmail.com>
2023-10-01 17:59:44 +0600

    production enabled

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-const production = false
+const production = true
 export const API_DOMAIN = production?"https://qwikit-grocery.onrender.com":"http://localhost:3000"
 export const API_URL = `${API_DOMAIN}/api`
